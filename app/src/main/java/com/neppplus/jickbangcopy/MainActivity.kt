bff9311a7f9cbe27212ab75c7f9652dcbcb48618
윤성노 <m3114@naver.com>
2021-11-27 12:26:40 +0900

    방 목록 변수 생성 실제 방 데이터들을 추가(직접 타이핑)

--- app/src/main/java/com/neppplus/jickbangcopy/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy/MainActivity.kt
@@ -6,11 +6,25 @@ import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
 
+    val mRoomList = ArrayList<RoomData>()
+
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRoomList.add(RoomData(8000,"서울시 동대문구", 5,"1번째 방입니다."))
+        mRoomList.add(RoomData(18000,"서울시 동대문구", 3,"2번째 방입니다."))
+        mRoomList.add(RoomData(28000,"서울시 서대문구", 2,"3번째 방입니다."))
+        mRoomList.add(RoomData(6000,"서울시 사대문구", 5,"1번째 방입니다."))
+        mRoomList.add(RoomData(5000,"서울시 동대문구", 3,"2번째 방입니다."))
+        mRoomList.add(RoomData(9000,"서울시 도대문구", 2,"2번째 방입니다."))
+        mRoomList.add(RoomData(7000,"서울시 동대문구", 1,"3번째 방입니다."))
+        mRoomList.add(RoomData(18000,"서울시 북대문구", 0,"4번째 방입니다."))
+        mRoomList.add(RoomData(28000,"서울시 남대문구", -1,"2번째 방입니다."))
+        mRoomList.add(RoomData(38000,"서울시 동문구", -1,"3번째 방입니다."))
+
     }
 }
