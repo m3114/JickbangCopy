0eb27bb5bfb9e80ed178c15c4390879355811839
윤성노 <m3114@naver.com>
2021-11-27 15:56:35 +0900

    handler기능 추가

--- app/src/main/java/com/neppplus/jickbangcopy/SplashActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy/SplashActivity.kt
@@ -1,11 +1,27 @@
 package com.neppplus.jickbangcopy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+//2.5초 잏에 화면 전환으로 지연시켜보자
+        val myHandler = Handler(Looper.getMainLooper())// 메인쓰레드소환(ui전담쓰레드)
+
+        myHandler.postDelayed({
+          // 2.5초뒤에 실행할 일-= 메인화면으로 이동하기
+            val myIntent =  Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+
+        },2500)
+
+
     }
 }
