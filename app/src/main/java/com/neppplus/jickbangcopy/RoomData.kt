5e6b2ab72d1de2d5c99edaee32bae9d463a512c1
윤성노 <m3114@naver.com>
2021-11-27 12:08:51 +0900

    방테이터르 표현할 코트린 제작

---
+++ app/src/main/java/com/neppplus/jickbangcopy/RoomData.kt
@@ -0,0 +1,15 @@
+package com.neppplus.jickbangcopy
+
+import android.location.Address
+import java.io.FileDescriptor
+
+class RoomData(
+    val price : Int,
+    val address: String,
+    val floor:Int,
+    val descriptor: String
+
+
+){
+
+}
