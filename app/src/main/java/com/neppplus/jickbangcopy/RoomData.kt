5d9d6593a3263c79df81c2a76429b19365da67f6
윤성노 <m3114@naver.com>
2021-11-27 13:37:40 +0900

    방데이터 기능 추가 - 층수에 따른 스트링 가공 어댑터에서 가져다 사용

--- app/src/main/java/com/neppplus/jickbangcopy/RoomData.kt
+++ app/src/main/java/com/neppplus/jickbangcopy/RoomData.kt
@@ -9,7 +9,25 @@ class RoomData(
     val floor:Int,
     val descriptor: String
 
-
 ){
+
+//    방 데이터 기능 : 가격을 가공해서 스트링으로 주는 기능= 리스트뷰에서 , 상세화면두군데에서 사용
+
+//    추가기능 : 층수도 가공해서 스트링으로 주자.
+
+      fun getFormattedFloor() : String{
+
+          if(this.floor > 0){
+              return "${this.floor}층"
+          }
+          else if(this.floor == 0){
+              return "반지하"
+          }
+          else{
+              return "지하 ${-this.floor}층" // -2 : 지하2층
+          }
+
+
+    }
 
 }
--- app/src/main/java/com/neppplus/jickbangcopy/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy/adapters/RoomAdapter.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.core.R
+import android.widget.TextView
+import com.neppplus.jickbangcopy.R
 import com.neppplus.jickbangcopy.RoomData
+import java.util.zip.Inflater
 
 class RoomAdapter(
-    val  mContext:Context,
+    val mContext:Context,
     val resId: Int,
     val mList : ArrayList<RoomData>
 )  : ArrayAdapter<RoomData>(mContext, resId, mList){
@@ -18,12 +20,25 @@ class RoomAdapter(
     val mInflater = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val  tempRow = convertView
+        var tempRow = convertView
 
         if(tempRow == null){
-            tempRow = mInflater.inflate( R.layout.room_list_item.null)
+            tempRow = mInflater.inflate(R.layout.room_list_item, null)
         }
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val txtPrice = row.findViewById<TextView>(R.id.txtPrice)
+        val txtAddressAndFloor = row.findViewById<TextView>(R.id.txtAddressAndFloor)
+        val txtDescriptor = row.findViewById<TextView>(R.id.txtDescriptor)
+
+
+        txtAddressAndFloor.text = "${data.address},${data.getFormattedFloor()}"
+
+
+        txtDescriptor.text = data.descriptor
+
         return  row
     }
 
